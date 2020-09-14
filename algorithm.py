5e04d7214782e1f5fdcc68d16213f091543a3548
Shway Wang <shuwei4@ualberta.ca>
2020-09-15 06:41:11 +0800

    separate files

--- algorithm.py
+++ algorithm.py
@@ -8,19 +8,24 @@ class Solver:
 	def __init__(self, huarongdao):
 		self.huarongdao = huarongdao
 
-	def isSuccess():
+	def isSuccess(self):
 		return self.zhen.caoCao.position == [2, 4]
 
+	def solve(self):
+		
+
 def main():
-	
 	# Zhen form:
-	zhen = TZYY()
+	zhen = TZYY()	# from games.py
 	# init the puzzle object:
 	huarongdao = Huarongdao(zhen)
 	# display the puzzle:
 	huarongdao.display();
 	# solve the puzzle:
-
+	Solver(huarongdao).solve()
+	# display solving result:
+	print("success!")
+	huarongdao.display()
 
 if __name__ == '__main__':
 	main()
